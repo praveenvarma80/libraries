85c8dfe17000de2891a237037290c319412601f0
praveenvarma80 <praveenvarma124547@gmail.com>
2024-10-24 16:37:56 +0530

    Update math.groovy

--- vars/math.groovy
+++ vars/math.groovy
@@ -1,4 +1,16 @@
-def downloads(repo)
+def gitDownload(repo)
 {
-  
+    git 'https://github.com/praveenvarma80/${repo}.git'
+}
+def buildArtifact()
+{
+     sh " mvn package "
+}
+def deployTomcat(jobname,ip,context)
+{
+   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
+}
+def RunSelenium(jobname)
+{
+  sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
 }
